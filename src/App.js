1bcd62ad3a9b59c518e88d36b3171a3b8e8ca4d6
Marcone Faria <marconefaria@cpejr.com.br>
2020-10-14 16:18:24 -0300

    aula-01

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Routes from "./routes";
+
+function App() {
+  return <Routes/>;
+}
+
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Cadastro from "./pages/Cadastro";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+function Routes(){
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/home" component={Home} />
+                <Route path="/cadastro" component={Cadastro} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
