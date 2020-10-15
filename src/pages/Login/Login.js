3788740b7b27553bfbf736a6320c20ecc426d829
Marcone Faria <marconefaria@cpejr.com.br>
2020-10-14 21:30:37 -0300

    login-funcional

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Login.css";
 import { Form, Button } from "react-bootstrap";
+import {useHistory} from "react-router-dom";
+import "./Login.css";
 
 function Login(){
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+    const history = useHistory();
+
+    function login(){
+        alert("Bem vindo, "+ email +"!");
+        history.push("home");
+    }
+
     return (
         <div className="base">
             <div className="container">
@@ -11,12 +21,18 @@ function Login(){
                     <p>Quick note</p>
                     <Form className="inputs">
                         <Form.Group controlId="email">
-                            <Form.Control type="email" placeholder="Digite seu e-mail" />
+                            <Form.Control 
+                            type="email" 
+                            placeholder="Digite seu e-mail" 
+                            onChange={(e) => setEmail(e.target.value)}/>
                         </Form.Group>
                         <Form.Group controlId="senha">
-                            <Form.Control type="password" placeholder="Senha" />
+                            <Form.Control 
+                            type="password" 
+                            placeholder="Senha" 
+                            onChange={(e) => setSenha(e.target.value)}/>
                         </Form.Group>
-                        <Button variant="primary">Login</Button>
+                        <Button variant="primary" onClick = {login}>Login</Button>
                     </Form>
                     <style>@import url('https://fonts.googleapis.com/css2?family=Oleo+Script+Swash+Caps&display=swap');</style>
                 </div>
