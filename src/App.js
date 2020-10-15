dfdc054b91ec551b8428bb90adbb8a9ad286ce5a
Marcone Faria <marconefaria@cpejr.com.br>
2020-10-14 21:05:50 -0300

    html/css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Routes from "./routes";
+import "./global.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return <Routes/>;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Login.css";
+import { Form, Button } from "react-bootstrap";
 
 function Login(){
     return (
-        <div>
-            <h1>Login</h1>
-            <Link to="home">Home</Link>
-            <Link to="cadastro">Cadastro</Link>
+        <div className="base">
+            <div className="container">
+                <div>
+                    <p>Quick note</p>
+                    <Form className="inputs">
+                        <Form.Group controlId="email">
+                            <Form.Control type="email" placeholder="Digite seu e-mail" />
+                        </Form.Group>
+                        <Form.Group controlId="senha">
+                            <Form.Control type="password" placeholder="Senha" />
+                        </Form.Group>
+                        <Button variant="primary">Login</Button>
+                    </Form>
+                    <style>@import url('https://fonts.googleapis.com/css2?family=Oleo+Script+Swash+Caps&display=swap');</style>
+                </div>
+            </div>
         </div>
     );
 }
