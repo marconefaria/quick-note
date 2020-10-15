b173ed347af9a72986365fb4d96f7686b3c577fb
Marcone Faria <marconefaria@cpejr.com.br>
2020-10-14 22:11:25 -0300

    home-inicial

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Cadastro from "./pages/Cadastro";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Menu from "./pages/Menu";
 
 function Routes(){
     return(
@@ -12,8 +13,9 @@ function Routes(){
                 <Route path="/login" component={Login} />
                 <Route path="/home" component={Home} />
                 <Route path="/cadastro" component={Cadastro} />
+                <Route path="/temp" component={Menu} />
             </Switch>
-        </BrowserRouter>
+        </BrowserRouter> 
     );
 }
 
